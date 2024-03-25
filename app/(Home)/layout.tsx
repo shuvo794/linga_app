641443aaceb6794947f94364ecbcdb9e359b6d56
shuvo794 <shuvohossain794@gmail.com>
2024-03-25 13:50:05 +0600

    Home page setup

---
+++ app/(Home)/layout.tsx
@@ -0,0 +1,5 @@
+const HomePageLayout = () => {
+  return <div>layout !</div>;
+};
+
+export default HomePageLayout;
--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-green-500 font-semibold">Hello Lingo !</p>
-      <Button>Click Me</Button>
+      <h2>This is a Home Page</h2>
     </div>
   );
 }
