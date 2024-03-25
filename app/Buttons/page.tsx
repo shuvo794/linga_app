06a7fe635e68158141a5e14b4844bc22029177bc
shuvo794 <shuvohossain794@gmail.com>
2024-03-25 12:30:03 +0600

    setup button

--- app/Buttons/page.tsx
+++ app/Buttons/page.tsx
@@ -3,8 +3,11 @@ import { Button } from "@/components/ui/button";
 const page = () => {
   return (
     <div className="p-4 space-y-4 flex flex-col mx-w-[200px]">
-      <Button>Primary</Button>
-      <Button>Primary Outline </Button>
+      <Button>Default</Button>
+      <Button variant="parimary">Primary</Button>
+      <Button variant="primaryOutline"> Primary Outline </Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="secondaryOutline"> secondary Outline </Button>
     </div>
   );
 };
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,21 +9,21 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        default:
+          "bg-white text-black  border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100",
+        parimary:
+          "bg-sky-400 border-slate-200 text-primary-foreground hover:bg-sky-400/90 border-sky-500 active:border-b-0 ",
+        primaryOutline: "bg-white text-sky-400 hover:bg-slate-100",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-green-400 text-green-foreground hover:bg-green-400/90 border-green-500 active:border-b-0 ",
+        secondaryOutline: "bg-white text-green-400 hover:bg-slate-100",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-11 px-4 py-2",
+        sm: "h-9  px-3",
+        lg: "h-12  px-8",
         icon: "h-10 w-10",
+        rounded: "rounded-full",
       },
     },
     defaultVariants: {
