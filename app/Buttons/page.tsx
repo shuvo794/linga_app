502af59cdc5e23535660067e38e33091e16d6b6a
shuvo794 <shuvohossain794@gmail.com>
2024-03-25 11:57:52 +0600

    setup button

---
+++ app/Buttons/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div className="p-4 space-y-4 flex flex-col mx-w-[200px]">
+      <Button>Primary</Button>
+      <Button>Primary Outline </Button>
+    </div>
+  );
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <p className="text-green-500 font-semibold">Hello Lingo !</p>
+      <Button>Click Me</Button>
     </div>
   );
 }
