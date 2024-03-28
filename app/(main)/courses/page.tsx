2919f86d3cd571c341ad35c12b6836859f5dd8e7
shuvo794 <shuvohossain794@gmail.com>
2024-03-28 12:57:55 +0600

    send

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,7 +1,11 @@
-const Courses = () => {
+import { getCourses } from "@/db/queries";
+
+const Courses = async () => {
+  const data = await getCourses();
   return (
     <div className="h-full max-w-[912px] mx-auto">
       <h2 className="text-2xl font-bold text-neutral-700">courses</h2>
+      {JSON.stringify(data)}
     </div>
   );
 };
--- db/queries.ts
+++ db/queries.ts
@@ -1,1 +1,7 @@
 import { cache } from "react";
+import db from "./drizzel";
+
+export const getCourses = cache(async () => {
+  const data = await db.query.courses.findMany();
+  return data;
+});
