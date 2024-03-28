5e242c3e5d0f6134f15274828c378116432ac64d
shuvo794 <shuvohossain794@gmail.com>
2024-03-28 15:12:26 +0600

    send

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -7,10 +7,19 @@ type Props = {
   activeCoursesId: number;
 };
 
-export const List = ({}: Props) => {
+export const List = ({ courses, activeCoursesId }: Props) => {
   return (
-    <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))]">
-      List
+    <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
+      {courses.map((course) => (
+        <Card
+          key={course.id}
+          id={course.id}
+          active={course.id === activeCoursesId}
+          title={course.title}
+          imageSrc={course.imageSrc}
+          disabled={false}
+        />
+      ))}
     </div>
   );
 };
