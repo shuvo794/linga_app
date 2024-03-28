a6cec3b74eb2b3d7893febd3046151c039b36028
shuvo794 <shuvohossain794@gmail.com>
2024-03-28 12:48:03 +0600

    send

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="h-full max-w-[912px] mx-auto">
+      <h2 className="text-2xl font-bold text-neutral-700">courses</h2>
+    </div>
+  );
+};
+
+export default page;
