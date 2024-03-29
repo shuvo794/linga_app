192293fdabc1cbfb89d757b19ece0bdd467b4685
shuvo794 <shuvohossain794@gmail.com>
2024-03-29 10:26:26 +0600

    send

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,12 @@
 import { cache } from "react";
 import db from "./drizzel";
+import { auth } from "@clerk/nextjs";
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
   return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
 });
