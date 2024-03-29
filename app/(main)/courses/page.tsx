6058aef8b1081a981e4c1e24f34a69e3fffcda2d
shuvo794 <shuvohossain794@gmail.com>
2024-03-29 10:58:45 +0600

    send

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -2,8 +2,12 @@ import { getCourses, getUserProgress } from "@/db/queries";
 import { List } from "./list";
 
 const Courses = async () => {
-  const courses = await getCourses();
-  const userProgress = await getUserProgress();
+  const coursesData = await getCourses();
+  const userProgressData = await getUserProgress();
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
   return (
     <div className="h-full max-w-[912px] mx-auto">
       <h2 className="text-2xl font-bold text-neutral-700 ml-3">
