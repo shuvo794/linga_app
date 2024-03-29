d8603e9ba34d7906e2dd538f55717efc6877da8c
shuvo794 <shuvohossain794@gmail.com>
2024-03-29 10:12:43 +0600

    send

--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
@@ -6,7 +7,11 @@ export const courses = pgTable("courses", {
   imageSrc: text("image_src").notNull(),
 });
 
-export const user_progress = pgTable("user_progress", {
+export const coursesRelationships = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}));
+
+export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(),
   userName: text("user_name").notNull().default("User"),
   userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
