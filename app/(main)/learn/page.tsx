da010630fba122c0125fba35ba2e6acbb44c2ec2
shuvo794 <shuvohossain794@gmail.com>
2024-03-29 11:09:02 +0600

    send

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -4,10 +4,15 @@ import { Header } from "./Header";
 
 import { UserProgress } from "@/components/User-Progress/User-Progress";
 import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
 const learnPage = async () => {
   const UserProgressData = getUserProgress();
   const [userProgress] = await Promise.all([UserProgressData]);
+
+  if (!userProgress || userProgress?.activeCourseId) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWorper>
