96600967ed3ce662e0175da7149cd4aeeee033b4
shuvo794 <shuvohossain794@gmail.com>
2024-03-30 11:02:36 +0600

    work a learn page

--- app/(main)/courses/loading.tsx
+++ app/(main)/courses/loading.tsx
@@ -2,8 +2,8 @@ import { Loader } from "lucide-react";
 
 const loading = () => {
   return (
-    <div className="h-full w-full flex justify-center items-center">
-      <Loader className="animate-spin h-5 w-5 text-muted-foreground" />
+    <div className="h-full w-full flex justify-center items-center ">
+      <Loader className="animate-spin h-5 w-5 text-muted-foreground " />
     </div>
   );
 };
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -17,14 +17,14 @@ const learnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWorper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
-          harts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          harts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubcription={false}
         />
       </StickyWorper>
       <FeedWorper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWorper>
     </div>
   );
