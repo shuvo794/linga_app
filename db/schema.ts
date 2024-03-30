9464fdb7574db8812a142aa5ff0f12255b27390c
shuvo794 <shuvohossain794@gmail.com>
2024-03-30 14:20:20 +0600

    work a seed file

--- db/schema.ts
+++ db/schema.ts
@@ -36,7 +36,7 @@ export const unitsRelationships = relations(units, ({ many, one }) => ({
     fields: [units.courseId],
     references: [courses.id],
   }),
-  lesson: many(lessons),
+  lessons: many(lessons),
 }));
 
 export const lessons = pgTable("lessons", {
