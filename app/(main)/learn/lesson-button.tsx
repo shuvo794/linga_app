2988c8f6da8c3c29c81f4d5f08c7eec341208da3
shuvo794 <shuvohossain794@gmail.com>
2024-03-31 16:19:27 +0600

    send

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -16,5 +16,18 @@ export const LessonButton = ({
   current,
   percentage,
 }: Props) => {
+  const cycleLength = 8;
+  const cycleIndex = index % cycleLength;
+  let indentionalLevel;
+  if (cycleIndex <= 2) {
+    indentionalLevel = cycleIndex;
+  } else if (cycleIndex <= 4) {
+    indentionalLevel = 4 - cycleIndex;
+  } else if (cycleIndex <= 6) {
+    indentionalLevel = 4 - cycleIndex;
+  } else {
+    indentionalLevel = cycleIndex - 8;
+  }
+
   return <div>{id}</div>;
 };
