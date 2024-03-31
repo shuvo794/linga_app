c910eca33975fbfebb5f3f18b5d6340eefd561bc
shuvo794 <shuvohossain794@gmail.com>
2024-03-31 14:57:56 +0600

    work a unit  page

--- app/(main)/learn/unit-banner.tsx
+++ app/(main)/learn/unit-banner.tsx
@@ -4,5 +4,12 @@ type Props = {
 };
 
 export const UnitBanner = ({ title, discreption }: Props) => {
-  return <div></div>;
+  return (
+    <div className="w-full rounded-md p-5 text-white items-center justify-between bg-green-500 mb-10 ">
+      <div className="space-y-2.5">
+        <h1 className="font-bold text-lg">{title}</h1>
+        <p>{discreption}</p>
+      </div>
+    </div>
+  );
 };
