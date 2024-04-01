eacd03b3d027f78cce92e468e625df28c8c05818
shuvo794 <shuvohossain794@gmail.com>
2024-04-01 13:21:31 +0600

    work a layout  page

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,3 +1,5 @@
+import { X } from "lucide-react";
+
 type Props = {
   hearts: number;
   percentage: number;
@@ -7,7 +9,7 @@ type Props = {
 export const Header = ({ hearts, percentage, hasActiveSubcription }: Props) => {
   return (
     <header className="lg:pt-[50px] pt-20 px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
-      Header
+      <X className="text-slate-500 hover:opacity-75 transition cursor-pointer" />
     </header>
   );
 };
