1b10ccc1623f2b70a5104840355f1ed7f0c8956a
shuvo794 <shuvohossain794@gmail.com>
2024-04-01 12:57:46 +0600

    work a Quiz  page

---
+++ app/lesson/header.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  hearts: number;
+  percentage: number;
+  hasActiveSubcription: boolean;
+};
+
+export const Header = ({ hearts, percentage, hasActiveSubcription }: Props) => {
+  return <div>Header</div>;
+};
