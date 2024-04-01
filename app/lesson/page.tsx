7dd180c90bfba85d850db67be6df93e5d40f34b6
shuvo794 <shuvohossain794@gmail.com>
2024-04-01 12:05:42 +0600

    work a lesson page

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,4 +1,17 @@
-const lessonPage = () => {
+import { getLesson, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+
+const lessonPage = async () => {
+  const lessonData = await getLesson();
+  const userProgressData = await getUserProgress();
+  const [lesson, userProgress] = await Promise.all([
+    lessonData,
+    userProgressData,
+  ]);
+
+  if (!lesson || !userProgress) {
+    redirect("/learn");
+  }
   return (
     <div>
       <h2>This is a lesson page</h2>
