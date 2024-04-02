99d279392d94591f58956f7499bff7ea1f5c45ed
shuvo794 <shuvohossain794@gmail.com>
2024-04-02 19:50:11 +0600

    depoly

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 type Props = {
   title: string;
-  id: number;
+  id?: number;
   imageSrc: string;
   onClick: (id: number) => void;
   disabled?: boolean;
@@ -21,7 +21,7 @@ export const Card = ({
 }: Props) => {
   return (
     <div
-      onClick={() => onClick(id)}
+      onClick={() => id !== undefined && onClick(id)}
       className={cn(
         "h-full ml-3 border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col justify-between items-center p-3 pb-6 min-h-[217px] min-w-[210px]",
         disabled && "pointer-events-none opacity-50"
