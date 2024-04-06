9623c174513d2f2ef2f4b0a812dbe78cf32b6adb
shuvo794 <shuvohossain794@gmail.com>
2024-04-06 10:32:20 +0600

    work parcentage

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -15,7 +15,6 @@ import { lessons, units as unitsSchema } from "@/db/schema";
 
 const learnPage = async () => {
   const UserProgressData = getUserProgress();
-
   const unitData = getUnits();
   const courseProgressData = getCourseProgress();
   const lessonPercentageData = getLessonPercentage();
