0e039e3cba997249850e92d348ce7fac8a7031ee
shuvo794 <shuvohossain794@gmail.com>
2024-04-06 12:00:10 +0600

    work a exit model section

--- components/models/exits-model.tsx
+++ components/models/exits-model.tsx
@@ -4,6 +4,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -11,6 +12,7 @@ import { useExistModel } from "@/store/use-exist-model";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
 
 export const ExistModel = () => {
   const router = useRouter();
@@ -35,6 +37,29 @@ export const ExistModel = () => {
             You&apos;re about to leave the lesson.Are you sure ?
           </DialogDescription>
         </DialogHeader>
+        <DialogFooter className="mb-4">
+          <div className="flex flex-col gap-y-2 w-full">
+            <Button
+              variant="parimary"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              Keep Learning
+            </Button>
+            <Button
+              variant="dangerOutline"
+              className="w-full"
+              size="lg"
+              onClick={() => {
+                close();
+                router.push("/learn");
+              }}
+            >
+              End sesstion
+            </Button>
+          </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
